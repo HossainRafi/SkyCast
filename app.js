06b7e37fe81b62b7980a5330a2e069bde29c6acc
HossainRafi <hossainrafi71@gmail.com>
2023-06-10 20:17:12 +0600

    convert time zone and others

--- app.js
+++ app.js
@@ -17,9 +17,19 @@ let weather__pressure = document.querySelector(".weather__pressure");
 // Convert date & time
 function convertTimeStamp(timestamp, timezone) {
   const convertTimezone = timezone / 3600; // convert seconds to hours
-
   const date = new Date(timestamp * 1000);
-
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(
+      convertTimezone
+    )}`,
+    hour12: true,
+  };
   return date.toLocaleString("en-US", options);
 }
 
